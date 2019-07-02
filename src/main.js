b0a1bed89abfc60d86ac4095a5ef235af320ec94
ismail-ahmadi <ahmadi.ismail92@gmail.com>
2019-07-02 21:36:14 +0700

    add accordion feature

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+// console.log('Hello, world!');
+const accs = Array.from(document.querySelectorAll('.accordion'));
+// console.log(accs);
+function newFunction() {
+  this.classList.toggle('accordion--active');
+  //   this.classList.remove('accordion--active');
+}
+accs.forEach(acc => acc.addEventListener('click', newFunction));
