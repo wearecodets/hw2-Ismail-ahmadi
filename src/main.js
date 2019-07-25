fdaf02d3445e7028a16777207c01212db575ab4e
ismail-ahmadi <ahmadi.ismail92@gmail.com>
2019-07-25 18:27:09 +0700

    revise the codes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,55 @@
+// const amount = document.querySelector('.form__total span');
+// const subButton = document.querySelector('button[type="submit"]');
+// const buttonAmount = document.querySelector('button span');
+// const inputs = Array.from(document.querySelectorAll('input[type="radio"]'));
+// const annual = document.querySelector('input[value="Annually"]');
+// const monthly = document.querySelector('input[value="Monthly"]');
+const accordions = document.querySelectorAll('.accordion');
+const activeClass = 'accordion--active';
 
-const amount = document.querySelector('.form__total span');
-const subButton = document.querySelector('button[type="submit"]');
+const monthlyCost = 30;
+const annualCost = 25;
+let amount = 0;
 
-const buttonAmount = document.querySelector('button span');
+const form = document.querySelector('.form');
+const total = document.querySelector('.form__total');
+const paypalLink = document.querySelector('a.form__cc-heading');
+
 // console.log(buttonAmount.textContent);
 // console.log(subButton.disabled);
 // console.log(amount);
-if (amount.textContent != '$0') {
-  subButton.disabled = false;
-  buttonAmount.textContent = amount.textContent;
+// <--------------- Paymeent Plan --------------->
+function calculateTotal() {
+  const frequency = form.plan.value;
+  amount = frequency === 'Monthly' ? monthlyCost : annualCost * 12;
 }
+
+function updateTotal() {
+  form.submit.disabled = false;
+  total.textContent = `$${amount}`;
+  form.submit.textContent = `Pay $${amount} Now`;
+  paypalLink.setAttribute('href', `https://paypal.me/nikosolihin/${amount}`);
+}
+
+form.plan.forEach(function(plan) {
+  plan.addEventListener('change', function() {
+    calculateTotal();
+    updateTotal();
+  });
+});
+// function changeCost() {
+//   if (annual.checked === true) {
+//     amount.textContent = '300';
+//   } else {
+//     amount.textContent = '30';
+//   }
+//   if (amount.textContent !== '0') {
+//     subButton.disabled = false;
+//     buttonAmount.textContent = amount.textContent;
+//   }
+// }
+// inputs.forEach(input => input.addEventListener('click', changeCost));
+
 // console.log('Hello, world!');
 // const total = document.querySelector('.form__total');
 // console.log(total);
@@ -27,38 +67,40 @@ if (amount.textContent != '$0') {
 // // radio.forEa.addEventListener('click', radioPayment());
 // const amount = document.querySelector('.form__total span');
 // console.log(amount);
-const inputs = Array.from(document.querySelectorAll('input[type="radio"]'));
 
-const annual = document.querySelector('input[value="Annually"]');
-const monthly = document.querySelector('input[value="Monthly"]');
-
-function changeCost() {
-  if (annual.checked === true) {
-    amount.textContent = '300';
-  } else {
-    amount.textContent = '30';
-  }
-}
-inputs.forEach(input => input.addEventListener('click', changeCost));
-
-const accs = Array.from(document.querySelectorAll('.accordion'));
-// console.log(accs);
-function newFunction() {
-  this.classList.toggle('accordion--active');
-  //   this.classList.remove('accordion--active');
-}
-accs.forEach(acc => acc.addEventListener('click', newFunction));
-console.log('Hello, world!');
+// const accs = Array.from(document.querySelectorAll('.accordion'));
+// // console.log(accs);
+// function newFunction() {
+//   this.classList.toggle('accordion--active');
+//   //   this.classList.remove('accordion--active');
+// }
+// accs.forEach(acc => acc.addEventListener('click', newFunction));
+// console.log('Hello, world!');
 // const amount = document.querySelector('.form__total span');
 // console.log(amount);
-const paypalLink = document.querySelector('a');
-// console.log(paypalLink.removeAttribute('href'));
-// paypalLink.href = 'www.goolge.com';
-function paypalweb() {
-  paypalLink.removeAttribute('href');
-  paypalLink.setAttribute(
-    'href',
-    `https://paypal.me/nikosolihin/${amount.textContent}`
-  );
-}
-paypalLink.addEventListener('click', paypalweb);
+// <----------- accodion code here below --------->
+accordions.forEach(function(item) {
+  item.addEventListener('click', function() {
+    if (item.classList.contains(activeClass)) {
+      // I'm currently active, so just close myself.
+      item.classList.remove(activeClass);
+    } else {
+      // I'm not active, so close the other first.
+      accordions.forEach(function(others) {
+        others.classList.remove(activeClass);
+      });
+      // Then activate myself.
+      item.classList.add(activeClass);
+    }
+  });
+});
+// < ----------- Paypal Link fix ------------ >
+// const paypalLink = document.querySelector('.form__right a img');
+// console.log(paypalLink);
+// // console.log(paypalLink.removeAttribute('href'));
+// // paypalLink.href = 'www.goolge.com';
+// function paypalweb() {
+//   paypalLink.removeAttribute('href');
+//   paypalLink.setAttribute('href', `https://paypal.me/nikosolihin/${amount}`);
+// }
+// paypalLink.addEventListener('click', paypalweb);
